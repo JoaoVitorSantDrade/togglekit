4dc311299e1bd19ebc97df5e7d6891f1d94036e6
Joao Vitor Santos <jvitorsantdrade@gmail.com>
2025-10-31 18:33:09 -0300

    feat: Update dependencies and implement basic API server
    - Added dependencies for tokio, uuid, axum, serde, serde_json, config, tracing, and tracing-subscriber in the workspace.
    - Configured togglekit-api to use axum and tokio with a basic GET route.
    - Implemented logging with tracing in the main function of togglekit-api.
    - Updated togglekit-core to include serde, tracing, and thiserror dependencies.
    - Added a feature flag evaluation function in togglekit-core with a corresponding test.
    - Introduced a docker-compose.yml file to set up PostgreSQL and Redis services for the project.

--- togglekit-api/src/main.rs
+++ togglekit-api/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get, routing::post};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "togglekit_api=debug,togglekit_core=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::info!("Starting ToggleKit API server...");
+
+    let app: Router = Router::new().route("/", get(async || "teste"));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
--- togglekit-core/src/lib.rs
+++ togglekit-core/src/lib.rs
@@ -1,3 +1,16 @@
+use tracing::info;
+
+pub fn is_enabled(flag_name: &str, context: Option<()>) -> bool {
+    info!(flag_name, "Evaluating feature flag");
+    // TODO:
+    // 1. Receber o `Ruleset` (conjunto de regras) do cache.
+    // 2. Encontrar a `flag_name` no ruleset.
+    // 3. Avaliar as regras (Percentage, Segmentos) contra o `context`.
+    match flag_name {
+        "new-dashboard" => true,
+        _ => false,
+    }
+}
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -10,5 +23,13 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_flag_evaluation() {
+        let context = None; // Contexto vazio por enquanto
+
+        assert_eq!(is_enabled("new-dashboard", context), true);
+        assert_eq!(is_enabled("other-flag", context), false);
     }
 }
